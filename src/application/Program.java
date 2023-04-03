d89a63c753044d8cda911b28636550b474aea7e4
Alexsander Kafka <alexsanderkafka2001alex@gmail.com>
2023-04-03 13:47:45 -0300

    parte final do tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,14 @@
 package application;
 
-import boardgame.Board;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Board b = new Board(8, 8);
+		ChessMatch c = new ChessMatch();
+		
+		UI.printBoard(c.getPieces());
 		
 	}
 
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,5 +1,7 @@
 package boardgame;
 
+import chess.ChessPiece;
+
 public class Board {
 
 	private int rows;
@@ -27,6 +29,18 @@ public class Board {
 	public void setColumns(int columns) {
 		this.columns = columns;
 	}
+	
+	public Piece piece(int row, int column) {
+		return pieces[row][column];
+	}
+	
+	public Piece piece(Position position) {
+		return pieces[position.getRow()][position.getColumn()];
+	}
+
+
+	
+	
 	
 	
 	
