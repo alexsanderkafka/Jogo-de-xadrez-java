5bb686fbcfc7c2a31ff8395840cc14199aa9f00e
Alexsander Kafka <alexsanderkafka2001alex@gmail.com>
2023-04-05 10:17:20 -0300

    Adicionado uma lista de peças capturadas

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 import chess.ChessException;
@@ -14,11 +16,12 @@ public class Program {
 		
 		Scanner sc = new Scanner(System.in);
 		ChessMatch c = new ChessMatch();
+		List<ChessPiece> captured = new ArrayList<>();
 		
 		while(true) {
 			try {
 				UI.clearScreen();
-				UI.printMatch(c);
+				UI.printMatch(c, captured);
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
@@ -32,6 +35,10 @@ public class Program {
 				ChessPosition target = UI.readChessPosition(sc);
 			
 				ChessPiece capturedPiece = c.performChessMove(source, target);
+				
+				if (captured != null) {
+					captured.add(capturedPiece);
+				}
 			}
 			catch(ChessException e) {
 				System.out.println(e.getMessage());
