fcffcd871128628865f7d887219ada3d73fef109
Alexsander Kafka <alexsanderkafka2001alex@gmail.com>
2023-04-06 14:47:57 -0300

    Adicionado a contagem de peças

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -8,7 +8,8 @@ import boardgame.Position;
 public abstract class ChessPiece extends Piece{
 	
 	private Color color;
-
+	private int moveCount;
+	
 	public ChessPiece(Board board, Color color) {
 		super(board);
 		this.color = color;
@@ -21,9 +22,23 @@ public abstract class ChessPiece extends Piece{
 		return color;
 	}
 	
+	public int getMoveCaunt() {
+		return moveCount;
+	}
+	
+	public void increaseMoveCount() {
+		moveCount++;
+	}
+	
+	public void decreaseMoveCount() {
+		moveCount--;
+	}
+	
 	protected boolean isThereOpponentPiece(Position position) {
 		ChessPiece p = (ChessPiece) getBoard().piece(position);
 		return p != null && p.getColor() != color;
 	}
+	
+	
 			
 }
